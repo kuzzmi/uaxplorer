bd9d359d8b57c3e103e808de5894f7b58425effb
Igor Kuzmenko <igor@kuzzmi.com>
2017-05-27 22:17:23 +0200

    Adds first version of working with auto.ria.ua API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,121 @@
+/*
+ * What car to buy?
+ *
+ * 1. I want to see a chart with:
+ *      Manufacturer/Count -> Manufacturer+Model/Count
+ *      To do this I need:
+ *      1. get list of car manufacturers
+ *      2. get list of average prices for each manufacturer and count of offers
+ */
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+    VictoryScatter,
+    VictoryChart,
+    VictoryTheme,
+    VictoryAxis,
+} from 'victory';
+
+// TODO: Move to separate service
+const processRequest = data => data.json();
+const baseUrl = 'http://api.auto.ria.com/';
+
+const API = {
+    request: options => fetch(baseUrl + options).then(processRequest),
+};
+// ------------------------------
+
+// TODO: Move to helpers
+const normalize = name => array => array.reduce((acc, obj) => ({ ...acc, [obj[name]]: obj }), {});
+
+const getByMark = mark => API.request(`average?marka_id=${ mark.value }`);
+// ------------------------------
+
+        // API.request('average?marka_id=79')
+        //     .then(data => {
+        //         const {
+        //             prices,
+        //             percentiles,
+        //         } = data;
+        //
+        //         const percentilesToRender = Object.keys(percentiles).map(k => ({
+        //             x: k,
+        //             y: percentiles[k],
+        //         }));
+        //
+        //         const pricesToRender =
+        //             prices.map(a => parseInt(a, 10))
+        //                   .sort((a, b) => a > b ? 1 : -1)
+        //                   .map((p, i) => ({ x: i, y: p }));
+        //
+        //         this.setState({
+        //             percentiles: percentilesToRender,
+        //             prices: pricesToRender,
+        //         });
+        //     });
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+        };
+    }
+
+    componentDidMount() {
+
+        const getMarkData = m =>
+            getByMark(m).then(data => {
+                this.setState(prevState => ({
+                    data: [
+                        ...prevState.data,
+                        {
+                            manufacturer: m.name,
+                            ...data,
+                        },
+                    ],
+                }));
+            });
+
+        API.request('categories/1/marks')
+            .then(marks => Promise.all(marks.map(getMarkData)));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <VictoryChart
+                    theme={ VictoryTheme.material }
+                    >
+                    <VictoryAxis
+                        style={{
+                            tickLabels: {
+                                fontSize: 10,
+                                angle: -90,
+                            },
+                        }}
+                        />
+                    <VictoryAxis
+                        dependentAxis
+                        style={{
+                            tickLabels: {
+                                fontSize: 10,
+                            },
+                        }}
+                        />
+                    <VictoryScatter
+                        data={ this.state.data }
+                        x="manufacturer"
+                        y="interQuartileMean"
+                        bubbleProperty="total"
+                        maxBubbleSize={ 10 }
+                        />
+                </VictoryChart>
+            </div>
+        );
+    }
 }
 
 export default App;
